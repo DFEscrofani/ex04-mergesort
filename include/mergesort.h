b505e447bee84d6dea6ea825543edbf4ba9cc440
DFEscrofani <escrofanidt@gmail.com>
2018-11-28 14:27:25 -0800

    commit start

---
+++ include/mergesort.h
@@ -0,0 +1,20 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 70342
+* Assignment: Merge sort
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @DaltonEscrofani
+*/
+#include <cstdint>
+
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+    template<class T>
+        void mergesort(T array[], size_t size) {
+
+        }
+
+}}}}
---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 70342
+* Assignment: Merge sort
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @DaltonEscrofani
+*/
+#include <cstdint>
+#include "mergesort.h"
+
+using edu::vcccd::vc::csv15::mergesort;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
